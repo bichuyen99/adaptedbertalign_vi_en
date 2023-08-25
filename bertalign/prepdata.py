be0ea813c2404c95cde50d2c0809412c597654ad
Uyen Vo <52061019+bichuyen99@users.noreply.github.com>
2023-08-25 19:17:15 +0300

    Update prepdata.py

--- bertalign/prepdata.py
+++ bertalign/prepdata.py
@@ -26,11 +26,11 @@ def preprocessing_data(source, word_vi, word_en, model_fasttext):
 	model_ft = fasttext.load_model(model_fasttext)
 
 	for i in range(len(doc_en)):
-	    sentences = nltk.sent_tokenize(doc_en[i])
-	    result = model_ft.predict(sentences[0])
-	    if result[0][0] == '__label__eng_Latn' and result[1][0] >= 0.99:
-	        pass
-	    else:
-	        doc_en[i] = ' '.join(sentences[1:])
+		sentences = nltk.sent_tokenize(doc_en[i])
+		result = model_ft.predict(sentences[0])
+		if result[0][0] == '__label__eng_Latn' and result[1][0] >= 0.99:
+			pass
+		else:
+			doc_en[i] = ' '.join(sentences[1:])
 
 		return doc_en, doc_vi
