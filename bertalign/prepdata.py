3775db0b9d52c0521b596d1e5fd702f6cc7523b2
Uyen Vo <52061019+bichuyen99@users.noreply.github.com>
2023-08-25 19:56:55 +0300

    Update prepdata.py

--- bertalign/prepdata.py
+++ bertalign/prepdata.py
@@ -1,8 +1,6 @@
 from bertalign.utils import *
 import fasttext
 from huggingface_hub import hf_hub_download
-import nltk
-nltk.download('punkt')
 
 def preprocessing_data(source, word_vi, word_en, model_fasttext):
 	# Load the data
@@ -22,16 +20,4 @@ def preprocessing_data(source, word_vi, word_en, model_fasttext):
 	doc_en = [sents_en[i] for i in range(len(sents_en)) if i not in empty_sents]
 	doc_vi = [sents_vi[i] for i in range(len(sents_vi)) if i not in empty_sents]
 
-
-	# Detect if the English document contains Vietnamese sentences, then delete these sentences.
-	model_ft = fasttext.load_model(model_fasttext)
-
-	for i in range(len(doc_en)):
-		sentences = nltk.sent_tokenize(doc_en[i])
-		result = model_ft.predict(sentences[0])
-		if result[0][0] == '__label__eng_Latn' and result[1][0] >= 0.99:
-			pass
-		else:
-			doc_en[i] = ' '.join(sentences[1:])
-
-		return doc_en, doc_vi
+	return doc_en, doc_vi
